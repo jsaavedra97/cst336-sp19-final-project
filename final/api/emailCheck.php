38dff7291869c6601c9d91d962bd09d8386cb889
Zach Strader <zstrader@csumb.edu>
2019-05-10 02:24:22 +0000

    sql command in bet_history.php

--- final/api/emailCheck.php
+++ final/api/emailCheck.php
@@ -1,5 +1,9 @@
 <?php
     session_start();
 	$email = $_SESSION['email_address'];
+	
+	// SELECT * FROM `history` NATURAL JOIN `user` WHERE history.email_address=user.email_address;
+	// This statement loads what we need for bet history but idk how to get it 
+	
     echo $email;
 ?>
